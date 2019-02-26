cef1eb442be08e7cbffa17ee986fad4ca795456b
Allie Beckman <alliemcanally@gmail.com>
2019-02-26 04:32:57 -0500

    Rough Draft of Pong AI Complete
    Computer gets easier to beat the lower the players score is and harder to beat the higher the players score.
    
    Very rough draft.

--- app/src/main/java/com/example/bridgesgametest/Pong.java
+++ app/src/main/java/com/example/bridgesgametest/Pong.java
@@ -21,14 +21,23 @@ public class Pong extends NGCKGame_AndroidVersion {
     private boolean moveUpDown; // Value true makes the ball move down, false moves the ball up.
     private int moveLeftRightCenter; // Value 1 moves the ball right, value -1 moves the ball left, value 0 the ball moves straight up or down.
 
+    private int reactionStrategy[][] = {{1, 50, 5}, {2, 45, 10}, {3, 40, 15}, {4, 35, 20}, {5, 30, 25}, {6, 25, 30}, {7, 20, 35}, {8, 15, 40}, {9, 10, 45}};
+    private int compScore = 0;
+    private int playerScore = 0;
+    private int reactionCountdown = reactionStrategy[8][0]; // Time between moves. The longer it takes to react the worse the computer plays.
+    private int errorCountdown = reactionStrategy[8][1]; // Time between computer paddle errors.
+    private int errorDuration = reactionStrategy[8][2]; // Duration of computer paddle errors.
+    private boolean error = false;
+    private int level = 0; // Iterates through the strategy array setting the computers play reaction and error counter.
+
     // Set up the first state of the game grid.
     private void initialize() {
         // paint screen
         for (int i = 0; i < 30; ++i) {
             for (int j = 0; j < 30; ++j) {
-                if (i%2 == 0){
+                if (i % 2 == j % 2) {
                     SetBGColor(i, j, NamedColor.gray);
-                }else{
+                } else {
                     SetBGColor(i, j, NamedColor.darkgray);
                 }
             }
@@ -74,13 +83,12 @@ public class Pong extends NGCKGame_AndroidVersion {
 
     public void handleBall() {
 
+        //SetFGColor(ballLoc[0], ballLoc[1], NamedColor.black);
         ballPrevLoc[0] = ballLoc[0]; // Set prev location val.
         ballPrevLoc[1] = ballLoc[1]; // Set prev location val.
         removeObject(ballPrevLoc[0], ballPrevLoc[1]); // Erase ball object from prev location.
 
         if (ballLoc[0] == 1) { // Ball has reached top of the board.
-            ballLoc[0]++;
-            moveUpDown = true;
             if (ballLoc[1] == compLoc[1] || ballLoc[1] == (compLoc[1] + 1) || ballLoc[1] == (compLoc[1] + 2)) { // Check for collision with computer paddle.
                 if (compLoc[1] < compPrevLoc[1]) { // If the computers paddle is moving left ball bounces right.
                     ballLoc[1]++;
@@ -91,12 +99,17 @@ public class Pong extends NGCKGame_AndroidVersion {
                 } else { // If the computers paddle is not moving when the ball collides the ball is not pushed in either direction.
                     moveLeftRightCenter = 0;
                 }
+                //SetFGColor(ballLoc[0], ballLoc[1], NamedColor.green);
             } else { // If the ball reaches x:0 on the grid and doesn't hit the computers paddle player wins.
-                // win
+                playerScore++;
+                if (level > 0) {
+                    level--;
+                }
+                //SetFGColor(ballLoc[0], ballLoc[1], NamedColor.green);
             }
+            ballLoc[0]++;
+            moveUpDown = true;
         } else if (ballLoc[0] == 28) { // Ball has reached bottom of the grid.
-            ballLoc[0]--;
-            moveUpDown = false;
             if (ballLoc[1] == loc[1] || ballLoc[1] == (loc[1] + 1) || ballLoc[1] == (loc[1] + 2)) { // Check for collision with player paddle.
                 if (loc[1] < prevLoc[1]) { // If the players paddle is moving left ball bounces right.
                     ballLoc[1]++;
@@ -107,9 +120,16 @@ public class Pong extends NGCKGame_AndroidVersion {
                 } else { // If the players paddle is not moving when the ball collides the ball is not pushed in either direction.
                     moveLeftRightCenter = 0;
                 }
+                //SetFGColor(ballLoc[0], ballLoc[1], NamedColor.red);
             } else { // If the ball reaches x:29 on the grid and doesn't hit the players paddle computer wins.
-                // lose
+                compScore++;
+                if (level < 8) {
+                    level++;
+                }
+                //SetFGColor(ballLoc[0], ballLoc[1], NamedColor.red);
             }
+            ballLoc[0]--;
+            moveUpDown = false;
         } else { // This is reached when the ball is not interacting with either paddle or the top and bottom of the grid.
 
             if (moveUpDown) { // Continues moving the ball away from the computers paddle.
@@ -130,11 +150,10 @@ public class Pong extends NGCKGame_AndroidVersion {
                 ballLoc[1]++;
             } else if (moveLeftRightCenter == -1) {
                 ballLoc[1]--;
-            }else{
+            } else {
                 // ball moves directly up or down
             }
         }
-
         grid.drawObject((ballLoc[0]), (ballLoc[1]), ballSymbol); // Draw ball object in new location.
     }
 
@@ -151,11 +170,67 @@ public class Pong extends NGCKGame_AndroidVersion {
             }
     }
 
+    private void handleComputerInput() {
+
+        if (error) {
+            compError();
+            if (errorDuration == 0) {
+                error = false;
+                errorCountdown = reactionStrategy[level][1];
+            }
+        } else {
+            compMove();
+            if (errorCountdown == 0) {
+                error = true;
+                errorDuration = reactionStrategy[level][2];
+            }
+        }
+    }
+
+    private void compMove() {
+        compPrevLoc[0] = compLoc[0];
+        compPrevLoc[1] = compLoc[1];
+
+        if (moveLeftRightCenter == -1) {
+            if (compLoc[1] > 0) {
+                compLoc[1]--;
+            }
+        }
+        if (moveLeftRightCenter == 1) {
+            if (compLoc[1] < 27) {
+                compLoc[1]++;
+            }
+        }
+        if (moveLeftRightCenter == 0) {
+            if (((compLoc[1] + 1) < ballLoc[1]) && (compLoc[1] < 27)) {
+                compLoc[1]++;
+            }
+            if (((compLoc[1] + 1) > ballLoc[1]) && (compLoc[1] > 0)) {
+                compLoc[1]--;
+            }
+        }
+    }
+
+    private void compError() {
+        if (compLoc[1] < 27) {
+            compLoc[1]++;
+        }
+    }
+
     // Game loop will run many times per second.
     public void GameLoop() {
         handleInput();
         drawPlayerPaddle();
         handleBall();
+
+        if (reactionCountdown <= 0) {
+            handleComputerInput();
+            drawComputerPaddle();
+
+            reactionCountdown = reactionStrategy[level][0];
+        }
+
+        reactionCountdown--;
     }
 
     // Called by the BufferedStart activity to begin running this game.
